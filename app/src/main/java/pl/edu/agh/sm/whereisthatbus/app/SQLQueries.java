304a726866d7a09285e974c2bd7ed324cc3d3450
erlkoing <peter.rybarczyk@gmail.com>
2014-05-31 00:28:37 +0200

    dodanie podpowiedzi podczas wypelniania pol: nazwa przystanki, numer linii

---
+++ app/src/main/java/pl/edu/agh/sm/whereisthatbus/app/SQLQueries.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.sm.whereisthatbus.app;
+
+/**
+ * Created by piotrek on 29.05.14.
+ */
+public class SQLQueries {
+    public final static String GET_ALL_BUS_STOPS = "SELECT Name FROM Stops";
+    public final static String GET_ALL_LINES = "SELECT Name FROM Lines";
+}
--- app/src/main/java/pl/edu/agh/sm/whereisthatbus/app/SearchActivity.java
+++ app/src/main/java/pl/edu/agh/sm/whereisthatbus/app/SearchActivity.java
@@ -1,9 +1,18 @@
 package pl.edu.agh.sm.whereisthatbus.app;
 
 import android.app.Activity;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Vector;
 
 
 public class SearchActivity extends Activity {
@@ -12,8 +21,58 @@ public class SearchActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getBusStopsName());
+        AutoCompleteTextView busStopsName = (AutoCompleteTextView) findViewById(R.id.busStopsName);
+        busStopsName.setAdapter(adapter);
+        busStopsName.setThreshold(1);
+
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getLinesNumber());
+        AutoCompleteTextView linesName = (AutoCompleteTextView) findViewById(R.id.linesName);
+        linesName.setAdapter(adapter2);
+        linesName.setThreshold(1);
+
+
     }
 
+    private List<String> getBusStopsName() {
+        Vector<HashMap<String, String>> queryResult = fetchDataFromDatabase(SQLQueries.GET_ALL_BUS_STOPS);
+
+        List<String> busStopsName = new ArrayList<String>();
+
+        for (HashMap<String, String> element: queryResult) {
+            busStopsName.add(element.get("Name"));
+        }
+
+        return busStopsName;
+    }
+
+    private List<String> getLinesNumber() {
+        Vector<HashMap<String, String>> queryResult = fetchDataFromDatabase(SQLQueries.GET_ALL_LINES);
+
+        List<String> linesNumber = new ArrayList<String>();
+
+        for (HashMap<String, String> element: queryResult) {
+            linesNumber.add(element.get("Name"));
+        }
+
+        return linesNumber;
+    }
+    
+
+
+    
+
+    private Vector<HashMap<String, String>> fetchDataFromDatabase(String query) {
+        BaseDatabaseRepository dataBase = new BaseDatabaseRepository(this);
+        dataBase.createDatabase();
+        dataBase.openDatabase(SQLiteDatabase.OPEN_READONLY);
+        Vector<HashMap<String, String>> data = dataBase.executeQuery(query, null);
+        dataBase.closeDatabase();
+
+        return data;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
