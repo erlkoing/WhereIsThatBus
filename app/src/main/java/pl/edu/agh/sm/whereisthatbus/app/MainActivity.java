e6232d7fc4115cac3da2545165fbd66d5180007b
erlkoing <peter24>
2014-09-06 17:44:58 +0200

    Dodanie komentarzy, reorganizacja kodu. Dodanie ladowania nazw przystankow i linii do sharedpreferences

--- app/src/main/java/pl/edu/agh/sm/whereisthatbus/app/MainActivity.java
+++ app/src/main/java/pl/edu/agh/sm/whereisthatbus/app/MainActivity.java
@@ -1,39 +1,56 @@
 package pl.edu.agh.sm.whereisthatbus.app;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.HashSet;
+import java.util.Set;
 
+/**
+ * Poczatkowe activity, w ktorym wybieramy co bedziemy robic: czy zglaszac czy szukac
+ */
 public class MainActivity extends Activity {
 
-    Button search;
-    Button report;
-
-    public void startSearchActivity() {
-        Intent i = new Intent(this, SearchActivity.class);
-        startActivity(i);
-    }
-
-    public void startReportActivity() {
-        Intent i = new Intent(this, ReportActivity.class);
-        startActivity(i);
-    }
+    private Button search;
+    private Button report;
+    private SharedPreferences prefs;
+    private DataBaseRepository db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        db = new DataBaseRepository(this);
+
+        // inicjalizacja SharedPreferences
+        prefs = this.getSharedPreferences("pl.edu.agh.sm.whereisthatbus", Context.MODE_PRIVATE);
+
+        // przypisanie elementow do obiektow
         search = (Button) findViewById(R.id.search);
         report = (Button) findViewById(R.id.report);
 
+        // ustawienie listenerow dla przycikow
+        setListeners();
 
+        // zaladowanie nazw przystankow i linii do SharedPreferences
+        loadBusStopsNames();
+        loadLinesNames();
+    }
 
+    /**
+     * Funkcja ustawia listenery dla przyciskow
+     */
+    private void setListeners() {
+        // przycisniecie przycisku przypisanego do "search" spowoduje wywołanie  funkcji startSearchActivity()
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -41,6 +58,7 @@ public class MainActivity extends Activity {
             }
         });
 
+        // przycisniecie przycisku przypisanego do "report" spowoduje wywołanie  funkcji startReportActivity()
         report.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -49,21 +67,59 @@ public class MainActivity extends Activity {
         });
     }
 
+    /**
+     * funkcja laduje nazwy przystankow do sharedpreferences
+     */
+    private void loadBusStopsNames() {
+        Set<String> busStopsNames = new HashSet<String>(db.getAllBusStopsNames());
+        prefs.edit().putStringSet("bus stops names", busStopsNames).commit();
+    }
+
+    /**
+     * funkcja laduje nazwy przystankow do sharedpreferences
+     */
+    private void loadLinesNames() {
+        Set<String> lineNames = new HashSet<String>(db.getAllLineNames());
+        prefs.edit().putStringSet("lines names", lineNames).commit();
+    }
+
+    /**
+     * Funkcja uruchamia SearchArctivity
+     */
+    private void startSearchActivity() {
+        Intent i = new Intent(this, SearchActivity.class);
+        startActivity(i);
+    }
+
+    /**
+     * Funkcja uruchamia ReportActivity
+     */
+    private void startReportActivity() {
+        Intent i = new Intent(this, ReportActivity.class);
+        startActivity(i);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+
+        boolean autoLocation = prefs.getBoolean("auto location", true);
+        if (!prefs.contains("auto location"))
+            prefs.edit().putBoolean("auto location", autoLocation).commit();
+
+        menu.findItem(R.id.auto_location).setChecked(autoLocation);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.auto_location) {
+            prefs.edit().putBoolean("auto location", !item.isChecked()).commit();
+            item.setChecked(!item.isChecked());
+
             return true;
         }
         return super.onOptionsItemSelected(item);
